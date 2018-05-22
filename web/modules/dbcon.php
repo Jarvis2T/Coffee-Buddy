ff37f543919778e59170864d722b0034dc30d1c6
Thanh Truong <jarvis.tr1109@gmail.com>
2018-05-22 16:07:51 +1200

    fix cleardb host

--- web/modules/dbcon.php
+++ web/modules/dbcon.php
@@ -3,7 +3,7 @@ $user = 'b73fe11f10e20d';
 $pass = 'e6e360ea';
 $db = 'heroku_661740760fa94b5';
 
-$db = mysqli_connect('localhost', $user, $pass, $db) ;
+$db = mysqli_connect('us-cdbr-iron-east-04.cleardb.net', $user, $pass, $db) ;
 #if($db){
 #	echo "connected";
 #}else{
