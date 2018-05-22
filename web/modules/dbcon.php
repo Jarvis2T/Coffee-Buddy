6d6b550d27daad6f7e3f8821b926419b033d86e5
Thanh Truong <jarvis.tr1109@gmail.com>
2018-05-22 15:32:31 +1200

    dbcon test

--- web/modules/dbcon.php
+++ web/modules/dbcon.php
@@ -1,7 +1,7 @@
 <?php  
-$user = 'root';
-$pass = '';
-$db = 'project';
+$user = 'b73fe11f10e20d';
+$pass = 'e6e360ea';
+$db = 'heroku_661740760fa94b5';
 
 $db = mysqli_connect('localhost', $user, $pass, $db) ;
 #if($db){
