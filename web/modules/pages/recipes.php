bd9ce1104c5974fc725a75e880f0293418e9180e
Thanh Truong <jarvis.tr1109@gmail.com>
2018-05-24 11:22:09 +1200

    grab img from s3 test

--- web/modules/pages/recipes.php
+++ web/modules/pages/recipes.php
@@ -9,7 +9,7 @@
 
 	<a href="main.php?p=recipedetails&id=<?php echo($row['id_coffee']) ?>" class="hover" >
 		<div class="recipelist">
-			<img src="admin/modules/coffeemanagement/uploads/<?php echo($row['coffeeimg']) ?>">
+			<img src="https://s3.us-east-2.amazonaws.com/thanh-img/americano.jpg<?php echo($row['coffeeimg']) ?>">
 			<h2><?php echo($row['coffeename']) ?></h2>
 			<p><?php echo($row['description']); ?></p>
 		</div>
