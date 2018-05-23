bfbc8076bc196bd7e359a88f678ee15b4b4fe945
Thanh Truong <jarvis.tr1109@gmail.com>
2018-05-24 11:54:55 +1200

    grab img from s3 test5

--- web/admin/modules/coffeemanagement/process.php
+++ web/admin/modules/coffeemanagement/process.php
@@ -1,11 +1,38 @@
 <?php 
 	include('../dbcon.php');
-	$id=$_GET['id'];
-	$coffeename=$_POST['coffeename'];
-	$description=$_POST['description'];
+	require '../../../../vendor/autoload.php';
+	$bucketName = 'thanh-img';
+	$IAM_KEY = 'AKIAJBMLP3OFGJPWRD3Q';
+	$IAM_SECRET = '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';
+	use Aws\S3\S3Client;
+	use Aws\S3\Exception\S3Exception;
+	$s3 = S3Client::factory(
+		array(
+			'credentials' => array(
+				'key' => $IAM_KEY,
+				'secret' => $IAM_SECRET
+			),
+			'version' => 'latest',
+			'region'  => 'us-east-2'
+		)
+	);
+
 	$coffeeimg=$_FILES['coffeeimg']['name'];
 	$coffeeimg_tmp=$_FILES['coffeeimg']['tmp_name'];
 	move_uploaded_file($coffeeimg_tmp,'uploads/'.$coffeeimg);
+	$s3->putObject(
+			array(
+				'Bucket'=>$bucketName,
+				'Key' =>  $coffeeimg,
+				'SourceFile' => $coffeeimg_tmp,
+				'StorageClass' => 'REDUCED_REDUNDANCY'
+	
+
+
+	$id=$_GET['id'];
+	$coffeename=$_POST['coffeename'];
+	$description=$_POST['description'];
+	
 	if (isset($_POST['add'])) {
 		
 		# add method
