01d5251a89125305e05dae588fe3c053f89531b1
Thanh Truong <jarvis.tr1109@gmail.com>
2018-05-28 11:06:18 +1200

    restore default

--- web/admin/modules/coffeemanagement/process.php
+++ web/admin/modules/coffeemanagement/process.php
@@ -1,51 +1,8 @@
 <?php 
 	include('../dbcon.php');
-	s3Client.
-	require '../../../../vendor/autoload.php';
-	$bucketName = 'thanh-img';
-	$IAM_KEY = 'AKIAJBMLP3OFGJPWRD3Q';
-	$IAM_SECRET = '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';
-	use Aws\S3\S3Client;
-	use Aws\S3\Exception\S3Exception;
-	try {
-		$s3 = new S3Client(['signature' => 'v4']);
-		$s3::factory(
-			array(
-				'credentials' => array(
-					'key' => $IAM_KEY,
-					'secret' => $IAM_SECRET
-				),
-				'version' => 'latest',
-				'region'  => 'us-east-2'
-			)
-		);
-	} catch (Exception $e) {
-		die("Error: " . $e->getMessage());
-	}
-
 	$coffeeimg=$_FILES['coffeeimg']['name'];
-	
-	$keyName = 'test_example/' . basename($_FILES["coffeeimg"]['tmp_name']);
-	$pathInS3 = 'https://s3.us-east-2.amazonaws.com/' . $bucketName . '/' . $keyName;
+	$coffeeimg_tmp=$_FILES['coffeeimg']['tmp_name'];
 	move_uploaded_file($coffeeimg_tmp,'uploads/'.$coffeeimg);
-	try {
-		// Uploaded:
-		$coffeeimg_tmp=$_FILES['coffeeimg']['tmp_name'];
-
-		$s3->putObject(
-			array(
-				'Bucket'=>$bucketName,
-				'Key' =>  $keyName,
-				'SourceFile' => $coffeeimg_tmp,
-				'StorageClass' => 'REDUCED_REDUNDANCY'
-			)
-		);
-	} catch (S3Exception $e) {
-		die('Error:' . $e->getMessage());
-	} catch (Exception $e) {
-		die('Error:' . $e->getMessage());
-	}
-
 	$id=$_GET['id'];
 	$coffeename=$_POST['coffeename'];
 	$description=$_POST['description'];
