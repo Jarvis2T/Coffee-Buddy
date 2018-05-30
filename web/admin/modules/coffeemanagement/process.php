9a7e2079a254f99cccfc921633eab67595beca6a
Thanh Truong <jarvis.tr1109@gmail.com>
2018-05-31 11:09:36 +1200

    add function to connect and upload img to s3 restore

--- web/admin/modules/coffeemanagement/process.php
+++ web/admin/modules/coffeemanagement/process.php
@@ -1,26 +1,21 @@
 <?php 
-	require('../../../../vendor/autoload.php');
-	use Aws\S3\S3Client;
-	use Aws\S3\Exception\S3Exception;
 	include('../dbcon.php');
 	include('../functions.php');
-
+	$coffeeimg=$_FILES['coffeeimg']['name'];
 	$id=$_GET['id'];
 	$coffeename=$_POST['coffeename'];
 	$description=$_POST['description'];
 
-		# Connect to s3 service
-	 
-	$coffeeimg=$_FILES['coffeeimg']['name'];
-	
-	
-	
+		// connect to s3
+	require('../../../../vendor/autoload.php');
+	use Aws\S3\S3Client;
+	use Aws\S3\Exception\S3Exception;
+
 	$bucketName = $_ENV["S3_BUCKET"];
 	$IAM_KEY = $_ENV["AWS_ACCESS_KEY_ID"];
 	$IAM_SECRET = $_ENV["AWS_SECRET_ACCESS_KEY"];
 
-	#s3connection($IAM_KEY,$IAM_SECRET);
-	$s3 = S3Client::factory(
+		$s3 = S3Client::factory(
 	    	array(
 	        	'credentials' => array(
 	            'key' => $IAM_KEY,
@@ -30,13 +25,27 @@
 	        'region' => 'us-east-1'
 	   		)    
 		);
+
 	$keyName = basename($coffeeimg);
 	$pathInS3 = 'https://s3.amazonaws.com/' . $bucketName . '/' . $keyName;
+
+	
 	if (isset($_POST['add'])) {
 		
-		# upload image to s3
-		
-        #s3upload($coffeeimg_tmp, $bucketName, $coffeeimg);
+		try {
+			$coffeeimg_tmp=$_FILES['coffeeimg']['tmp_name'];
+
+        	$s3->putObject(
+	            array(
+		            'Bucket' => $bucketName,
+		            'Key' => $keyName,
+		            'SourceFile' => $coffeeimg_tmp,
+		            'ACL' => 'public-read'
+	           	)
+        	);
+        } catch (Exeption $e) {
+        	echo $e->getMessage();
+        }		
 
         # add method	
 
@@ -44,7 +53,6 @@
 
 	}elseif (isset($_POST['edit'])) {
 		
-		# upload image to s3
 		try {
 			$coffeeimg_tmp=$_FILES['coffeeimg']['tmp_name'];
 
@@ -59,8 +67,6 @@
         } catch (Exeption $e) {
         	echo $e->getMessage();
         }
-	
-        #s3upload($coffeeimg_tmp, $bucketName, $coffeeimg);
 
 		# edit method
 		
